fe7e5fb0cb7a08c996342d940882977e34542a10
Victor Muhoro <victormuhoro7@gmail.com>
2024-01-14 22:40:43 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = new Set();
+  for (let num of array) {
+    if (seenNumbers.has(target - num)) {
+      return true; 
+    }
+    seenNumbers.add(num)
+
+  }
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+   Time complexity: O(n)
+   The function iterates through the array once, so its time complexity is linear in the size of the input array.
 */
 
 /* 
   Add your pseudocode here
+  1.Creates an empty hash set and name it seenNumbers
+  2.Iterate through the array
+  3.For each number, check if (target - current number) is in the set.
+  4.If yes, return true.
+  5.If no, add the current number to the set.
+  6.If the loop completes, return false.
 */
 
 /*
   Add written explanation of your solution here
+    The function uses a set to keep track of the numbers seen so far.
+     While iterating through the array, for each number, it checks if 
+     the complement of that number with respect to the target is already 
+     in the set. If yes, it means a pair has been found whose sum is equal
+      to the target, and the function returns true. If no such pair is found 
+      after iterating through the entire array, the function returns false.
+
 */
 
 // You can run `node index.js` to view these console logs
